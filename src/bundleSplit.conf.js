82bb5619931c06737c3c1733eb9baa2316e105be
Soul <a1009943858@gmail.com>
2022-06-16 14:21:09 +0800

    update: config

--- src/bundleSplit.conf.js
+++ src/bundleSplit.conf.js
@@ -1,6 +1,13 @@
 module.exports = {
   whiteList: Array.from(
-    new Set(['src/api', 'src/components', 'src/hooks', 'src/utils', 'app.json'])
+    new Set([
+      'index.js',
+      'src/api',
+      'src/components',
+      'src/hooks',
+      'src/utils',
+      'app.json',
+    ])
   ),
   blackList: [],
 };
