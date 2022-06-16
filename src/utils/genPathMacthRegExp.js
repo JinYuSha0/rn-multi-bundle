10da9f0b3486416a5936fcef639b621a2ad93481
Soul Shao <soul.shao>
2022-06-16 22:47:20 +0800

    fix

--- src/utils/genPathMacthRegExp.js
+++ src/utils/genPathMacthRegExp.js
@@ -1,5 +1,13 @@
+const path = require('path');
+const sep = path.sep;
+
 module.exports = function genPathMacthRegExp(pathList) {
   if (!pathList || pathList.length === 0) return /\s/;
-  const startsWithRegExp = pathList.map((reg) => `\\/?${reg}`).join('|');
-  return new RegExp(`\^(${startsWithRegExp})\/?([^\/]*)$`);
+  const startsWithRegExp = pathList
+    .map((pathname) =>
+      path.normalize(pathname).replace(new RegExp(`\\${sep}`, 'g'), `\\${sep}`)
+    )
+    .map((pathname) => `\\${sep}?${pathname}`)
+    .join('|');
+  return new RegExp(`\^(${startsWithRegExp})\\${sep}?([^\\${sep}]*)$`);
 };
