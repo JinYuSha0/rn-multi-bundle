10b4d7eb440881ccae4371d4514812a340c254d8
soul <shaojy@hua-cloud.com.cn>
2022-06-22 19:00:56 +0800

    fix

--- src/utils/genPathMacthRegExp.js
+++ src/utils/genPathMacthRegExp.js
@@ -4,10 +4,21 @@ const sep = path.sep;
 module.exports = function genPathMacthRegExp(pathList) {
   if (!pathList || pathList.length === 0) return /\s/;
   const startsWithRegExp = pathList
-    .map((pathname) =>
-      path.normalize(pathname).replace(new RegExp(`\\${sep}`, 'g'), `\\${sep}`)
-    )
-    .map((pathname) => `\\${sep}?${pathname}`)
+    .map((pathname) => {
+      let isDir = false;
+      try {
+        const stat = fs.lstatSync(pathname);
+        isDir = stat.isDirectory();
+      } catch {}
+      return [pathname, isDir];
+    })
+    .map(([pathname, isDir]) => [
+      path.normalize(pathname).replace(new RegExp(`\\${sep}`, 'g'), `\\${sep}`),
+      isDir,
+    ])
+    .map(([pathname, isDir]) => {
+      return `\\${sep}?${pathname}${isDir ? `\\${sep}.*` : ''}`;
+    })
     .join('|');
   return new RegExp(`\^(${startsWithRegExp})\\${sep}?([^\\${sep}]*)$`);
 };
